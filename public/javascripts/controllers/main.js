debe481521ffdff52f5054a3b72262f673c433f0
Moose <mostafa.saadat@outlook.com>
2017-03-01 18:26:40 -0500

    UI changes

--- public/javascripts/controllers/main.js
+++ public/javascripts/controllers/main.js
@@ -1,5 +1,5 @@
 angular.module('bc-easter-egg')
-    .controller('mainController', function($scope) {
+    .controller('mainController', function ($scope) {
 
         $scope.b1 = {
             num: 1,
@@ -8,14 +8,31 @@ angular.module('bc-easter-egg')
             prev: '0000000000000000000000000000000000000000000000000000000000000000',
             hash: 'hash would be here'
         }
-        
-        hash = function(block) {
+
+        hash = function (block) {
             var sha256 = new jsSHA("SHA-256", "TEXT");
             sha256.update(block.num + block.nonce + block.data + block.prev);
             return sha256.getHash("HEX");
         }
-        $scope.update = function(block) {
+        $scope.update = function (block) {
             console.log(block);
             block.hash = hash(block);
         }
     });
+
+$(document).ready(function () {
+    console.log('loaded');
+    // $('.bc-down-icon').click(function() {
+    //     console.log('clicked');
+    //     $('.help-slide-modal').addClass('bc-modal-show');
+    // });
+});
+
+function showHelp() {
+    console.log("clicked");
+    document.getElementById('help-slide').classList.remove('help-slide-hide');
+    document.getElementById('help-slide').classList.add('help-slide-show');
+    
+    document.getElementById('bc-chevron').style.display = 'none';
+    
+}
