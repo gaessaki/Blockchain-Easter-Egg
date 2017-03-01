6e2dead41d5cd28561a26a9e192868ff22cf815d
Mansib Rahman <mansib.rahman@outlook.com>
2017-03-01 16:22:23 -0500

    Added hashing function

--- public/javascripts/controllers/main.js
+++ public/javascripts/controllers/main.js
@@ -1,4 +1,7 @@
 angular.module('bc-easter-egg')
     .controller('mainController', function($scope) {
         
+        $scope.hash = function(block) {
+            return sjcl.hash.sha256(block.id + block.nonce + block.data + block.prev);
+        } 
     });
